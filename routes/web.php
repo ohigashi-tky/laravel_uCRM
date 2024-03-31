cba763a55f42cfe1d81f42b657062d82104280df
ohigashi05 <96708511+ohigashi05@users.noreply.github.com>
2024-03-31 21:26:48 +0900

    inertiatestクラス作成

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
+
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+    }
+);
+
+Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
