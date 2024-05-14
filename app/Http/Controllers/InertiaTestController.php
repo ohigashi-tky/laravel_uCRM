b9a98dbb0959641768199759fd8298d4e0cf8b8e
ohigashi-tky <roziura8114@yahoo.co.jp>
2024-05-14 23:33:40 +0900

    作業途中

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\InertiaTest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -14,5 +15,14 @@ class InertiaTestController extends Controller
 
     public function show($id = null): Response {
         return Inertia::render('Inertia/Show', ['id' => $id]);
+    }
+
+    public function store (Request $request) {
+        $inertiaTest = new InertiaTest;
+        $inertiaTest->title = $request->title;
+        $inertiaTest->content = $request->content;
+        $inertiaTest->save();
+
+        return to_route('inertia.index');
     }
 }
